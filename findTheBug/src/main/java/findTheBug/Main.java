fc2faeb927a49d1f6ec1a0e9d91374bfc59f8204
Lennart Van Damme <Lennart.VanDamme@realdolmen.com>
2016-10-07 15:19:05 +0200

    find the bug

---
+++ findTheBug/src/main/java/findTheBug/Main.java
@@ -0,0 +1,16 @@
+package findTheBug;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		final int [] INDEX_ARRAY = {0,1,2,3,4,6};
+		int [] values = {0,1,2,3,4};
+		
+		for(int i = 0; i < INDEX_ARRAY.length; i++){
+			values[INDEX_ARRAY[i]] = i;
+		}
+
+	}
+
+}
